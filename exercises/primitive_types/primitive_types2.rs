3f2d890ff6f4de42db1ed5ea2f68c871953b53c1
Schatten <177341921+Schatten-42@users.noreply.github.com>
2025-10-22 23:00:39 +0800

    mod 'your_character'

--- exercises/primitive_types/primitive_types2.rs
+++ exercises/primitive_types/primitive_types2.rs
@@ -21,9 +21,13 @@ fn main() {
         println!("Neither alphabetic nor numeric!");
     }
 
-    let your_character = '@';// Finish this line like the example! What's your favorite character?
+    let your_character = '𝄞';// Finish this line like the example! What's your favorite character?
     // Try a letter, try a number, try a special character, try a character
     // from a different language than your own, try an emoji!
+
+    println!("{}", your_character);           // 打印字符
+    println!("{:X}", your_character as u32);  // 十六进制码点：9673
+
     if your_character.is_alphabetic() {
         println!("Alphabetical!");
     } else if your_character.is_numeric() {
