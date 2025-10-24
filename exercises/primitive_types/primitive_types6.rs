00bddd338f20c583e8d19f21e6dcdd58a7c5d293
Schatten <177341921+Schatten-42@users.noreply.github.com>
2025-10-25 00:03:00 +0800

    1

--- exercises/primitive_types/primitive_types6.rs
+++ exercises/primitive_types/primitive_types6.rs
@@ -11,8 +11,9 @@
 fn indexing_tuple() {
     let numbers = (1, 2, 3);
     // Replace below ??? with the tuple indexing syntax.
-    let second = ???;
+    let second = numbers.1;
 
-    assert_eq!(2, second,
-        "This is not the 2nd number in the tuple!")
+    println!("{:?} is the second from {:?}", second, numbers);
+
+    assert_eq!(2, second,"This is not the 2nd number in the tuple!")
 }
