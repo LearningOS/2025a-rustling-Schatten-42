f76118b5221a55021ff21a5256eccc68db74d92a
Schatten <177341921+Schatten-42@users.noreply.github.com>
2025-10-29 15:53:15 +0800

    err2不错

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -18,17 +18,38 @@
 //
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a
 // hint.
+/*
+假设我们正在编写一款游戏，您可以在其中使用代币购买物品。所有物品成本
+5 个代币，每当您购买物品时，都需要支付 1 的手续费
+令 牌。游戏玩家将输入他们想要购买的物品数量，以及
+“total_cost”函数将计算代币的总成本。因为
+玩家输入数量，但是，我们得到它作为字符串——他们
+可能输入了任何东西，而不仅仅是数字！
+目前，此函数根本没有处理错误情况（也不是
+正确处理成功案例）。我们想做的是：如果我们调用
+非数字字符串上的“parse”函数，该函数将
+返回 'ParseIntError'，在这种情况下，我们希望立即返回
+我们的函数中的错误，而不是尝试乘法和相加。
+至少有两种方法可以实现这一点，它们都是正确的——但一种
+短了很多！
+*/
 
-// I AM NOT DONE
+
 
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    // #1
+    // let qty: i32 = item_quantity.parse::<i32>()?; // `?`作用：- 失败了 立即调用return返回 - 成功了，自动解包 Ok(i32) -> i32
+    // Ok(qty * 5 + 1)
+    
+    // #2
+   // 调用 .map() --> Err 会自动忽略.map()，透传出去
+    item_quantity.parse::<i32>().map(|qty| qty * 5 + 1)
 
-    Ok(qty * cost_per_item + processing_fee)
+    
 }
 
 #[cfg(test)]
--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 struct Point {
     x: i32,
@@ -12,9 +11,13 @@ struct Point {
 
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
+    // match y 会 move y （绑定到 Some(p)），会导致 y 后续不可用
+    // 解法1: match &y
+    // 解法2: Some(ref p)，表示只借用，不 move
 
-    match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+
+    match y { 
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
